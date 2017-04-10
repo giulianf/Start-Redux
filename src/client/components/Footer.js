4a6e57998939cb6f3a3a22cbbdea3d01c951afcf
Julien <julien.fumanti@gmail.com>
2017-04-10 15:09:26 +0200

    migrate class to component

--- src/client/components/Footer.js
+++ src/client/components/Footer.js
@@ -1,9 +1,9 @@
-var React = require('react');
+import React, { Component } from 'react';
 import { Grid, Row, Col} from 'react-bootstrap';
 import { Link } from 'react-router';
 import moment from 'moment';
 
-export default class Footer extends React.Component {
+export default class Footer extends Component {
   constructor (){
     super();
   }
@@ -12,31 +12,20 @@ export default class Footer extends React.Component {
       const year = moment().year();
 
     return (
-      <footer className="footer section-padding">
-        <Grid>
-          <Row>
-            <Col md={12} sm={12} xs={12} className="text-center" >
-              <div className="footer_content">
-                  <Col md={12} sm={2} xs={12} className="text-center">
-                    <Link to="/"><img src="/img/logo.png" alt="" width="150"/></Link>
-                  </Col>
-                <div className="footer_social">
-                  <ul>
-                    <li><a className="f_facebook  wow bounceInDown" target="_blank" href="https://www.facebook.com/pret.coup.de.pouce.katapulta/"><i className="fa fa-facebook"></i></a></li>
-                    <li><a className="f_twitter wow bounceInDown" data-wow-delay=".1s" href="#"><i className="fa fa-twitter"></i></a></li>
-                    <li><a className="f_google wow bounceInDown" data-wow-delay=".2s" href="#"><i className="fa fa-google-plus"></i></a></li>
-                    <li><a className="f_linkedin wow bounceInDown" data-wow-delay=".3s" href="#"><i className="fa fa-linkedin"></i></a></li>
-                    <li><a className="f_youtube wow bounceInDown" data-wow-delay=".4s" href="#"><i className="fa fa-youtube"></i></a></li>
-                    <li><a className="f_skype wow bounceInDown" data-wow-delay=".5s" href="#"><i className="fa fa-skype"></i></a></li>
-                  </ul>
-                </div>
-                <p>Ufiix&copy; {year}  est une marque déposée de Finauxa sa. All Rights Reserved.</p>
-              </div>
-
-            </Col>
-          </Row>
-        </Grid>
-      </footer>
+    <footer className="dark-footer">
+      <Grid>
+        <p className="footer-info">© {year} - <strong>Ufiix</strong> - Marque déposée de Finauxa sa. All Rights Reserved.
+          <span className="social pull-right">
+                <a href="#"><i className="fa fa-facebook"></i></a>
+                <a href="#"><i className="fa fa-twitter"></i></a>
+                <a href="#"><i className="fa fa-google-plus"></i></a>
+                <a href="#"><i className="fa fa-pinterest"></i></a>
+                <a href="#"><i className="fa fa-linkedin"></i></a>
+                <a href="#"><i className="fa fa-dribbble"></i></a>
+            </span>
+        </p>
+      </Grid>
+    </footer>
     );
   }
 }
